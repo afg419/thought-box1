7e4eba3b01c0dd8d8fde10a1f9be4e5b3bbd77bf
Aaron Greenspan <afg419@gmail.com>
2016-04-18 12:34:38 -0600

    Renders inline editing when asked for

--- app/assets/javascripts/components/_link.js.jsx
+++ app/assets/javascripts/components/_link.js.jsx
@@ -1,6 +1,6 @@
 var Link = React.createClass({
   getInitialState(){
-    return {content: this.props.content};
+    return {content: this.props.content, editable: false};
   },
 
   readUnread(){
@@ -28,13 +28,53 @@ var Link = React.createClass({
     );
   },
 
+  renderEdit(){
+    this.setState({editable: !this.state.editable}, ()=>{console.log("happened");});
+  },
+
+  editableEntries(){
+    var renderable;
+    if(this.state.editable){
+      renderable = (<div>
+        <input ref="title" type="text" value={this.state.content.title}></input>
+        <input ref="url" type="text" value={this.state.content.url}></input>
+        <button onClick={this.handleUpdate}>Update link</button>
+        </div>);
+    } else {
+      renderable = (<div>
+        <h4>{this.state.content.title}</h4>
+        <p>{this.state.content.url}</p>
+        </div>);
+    }
+    return renderable;
+  },
+
+  handleUpdate(){
+    var title = this.refs.title.value;
+    var url = this.refs.url.value;
+    $.ajax({
+      url: '/api/v1/links/' + this.props.content.id,
+      type: 'PATCH',
+      // data: {read: !this.state.content.read},
+      success: (link) => {
+        if(link){
+          this.setContent(link);
+          console.log(link);
+        } else {
+            this.setState({message: ""});
+          }
+        }
+      }
+    );
+  },
+
   render(){
     return(
       <div className={this.state.content.read} key={this.state.content.id}>
-        <h4>{this.state.content.title}</h4>
-        <p>{this.state.content.url}</p>
+        {this.editableEntries()}
         <p>Read: {""+this.state.content.read}</p>
         <button onClick={this.handleReadUnread}>{this.readUnread()}</button>
+        <button onClick={this.renderEdit}>Edit</button>
       </div>
     );
   }
