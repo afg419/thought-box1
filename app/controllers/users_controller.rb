f959a83773780477d6e74fc9ec48f181759c5af1
Aaron Greenspan <afg419@gmail.com>
2016-04-18 09:39:30 -0600

    User can create new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Created account and logged in #{@user.email}"
+      redirect_to user_path
+    else
+      flash[:notice] = "Something blew up"
+      render :new
+    end
+  end
+
+  def show
+    @user = current_user
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
---
+++ spec/features/new_user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "NewUserAccount", type: :feature do
+  # before(:each) do
+  #
+  # end
+
+  scenario "A logged in user can see email" do
+    expect(User.count).to eq 0
+
+    visit root_path
+    click_on "Sign Up"
+    fill_in "user_email", with: "afg419@gmail.com"
+    fill_in "user_password", with: "password"
+    click_on "Create new account"
+    expect(current_path).to eq user_path
+    expect(page).to have_content("Created account and logged in afg419@gmail.com")
+    expect(User.count).to eq 1
+  end
+end
